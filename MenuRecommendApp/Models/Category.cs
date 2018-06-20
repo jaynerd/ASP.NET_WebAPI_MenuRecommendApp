7a33cb6063ddb10fb3c04777aca3ec93bd8cd564
unknown <j4yn3rd@gmail.com>
2018-06-21 11:10:48 +1200

    env upload

---
+++ MenuRecommendApp/Models/Category.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace MenuRecommendApp.Models
+{
+    public abstract class Category : IComparable
+    {
+        public int Id { get; }
+        public string Name { get; }
+
+        protected Category() { }
+
+        protected Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static IEnumerable<T> GetAll<T>() where T : Category, new()
+        {
+            var type = typeof(T);
+            var fields = type.GetTypeInfo().GetFields(BindingFlags.Public |
+            BindingFlags.Static |
+            BindingFlags.DeclaredOnly);
+
+            foreach (var info in fields)
+            {
+                var instance = new T();
+                if (info.GetValue(instance) is T locatedValue)
+                {
+                    yield return locatedValue;
+                }
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Category otherValue))
+            {
+                return false;
+            }
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+            return typeMatches && valueMatches;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return Id.CompareTo(((Category)obj).Id);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1919740922;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            return hashCode;
+        }
+    }
+}
---
+++ MenuRecommendApp/Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MenuRecommendApp.Models
+{
+    public class Entry
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
